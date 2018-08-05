61216e13c73a33392897de0099f1f2455e591037
hongyoonsun <yoonsun.dev@gmail.com>
2018-08-06 03:51:57 +0900

    first commit

---
+++ template/MobileClose_files/_NavBar.js
@@ -0,0 +1,179 @@
+﻿var navbarInitializer = {}
+
+// DB 에서 메뉴를 조회하여 콜백 함수 호출
+navbarInitializer.initNavbar = function () {
+    $.ajax({
+        url: "/Home/GetTotalMenu",
+        type: "POST",
+        async: false,
+        contentType: "application/json; charset=utf-8",
+        success: function (result) {
+            if (result != null) {
+                navbarInitializer.setNavbar(result);
+            }
+        },
+        error: function (err) {
+            err = err + "";
+            alert('메뉴조회 실패 : ' + err);
+        }
+    });
+
+    $(".gnb-menulink").on("click", navbarInitializer.openMenu);
+}
+
+// DB 에서 메뉴들을 조회 한 후 호출되는 콜백 함수
+navbarInitializer.setNavbar = function(menuArr) {
+    // 사용하지 않는 메뉴 삭제
+    try {
+        //var usedMenuArr = menuArr.filter(function(){menu => menu.MenuUseYn == "Y"});
+        // var lMenuArr = usedMenuArr.filter(menu => menu.MenuLevel == 1);
+        //var mMenuArr = usedMenuArr.filter(menu => menu.MenuLevel == 2);
+        //var sMenuArr = usedMenuArr.filter(menu => menu.MenuLevel == 3);
+
+        var usedMenuArr = menuArr.filter(function(menu){return menu.MenuUseYn == "Y"});
+        var lMenuArr = usedMenuArr.filter(function(menu){return menu.MenuLevel == 1});
+        var mMenuArr = usedMenuArr.filter(function(menu){return menu.MenuLevel == 2});
+        var sMenuArr = usedMenuArr.filter(function (menu) {return  menu.MenuLevel == 3 });
+
+
+        // 소메뉴에서 지마켓 삭제
+        //var iacSMenuArr = sMenuArr.filter(menu => menu.SCodeName != "지마켓");
+        var iacSMenuArr = sMenuArr.filter(function (menu) { return menu.SCodeName != "지마켓" });
+
+        // 메뉴를 tree 구조로 변환
+        var appendedMmenuArr = navbarInitializer.appendSmenusToMmenus(iacSMenuArr, mMenuArr);
+        var appendedLmenuArr = navbarInitializer.appendMmenusToLmenus(appendedMmenuArr, lMenuArr);
+        navbarInitializer.appendLmenusToNavbar(appendedLmenuArr);
+    } catch (e) { }
+}
+
+
+
+// 소메뉴 목록을 대응하는 중메뉴 목록에 추가
+navbarInitializer.appendSmenusToMmenus = function (sMenuArr, mMenuArr) {
+    var resultMmenuArr = [];
+
+    for(var i = 0; i<mMenuArr.length;i++)
+    {
+        /*
+        var curMmenu = { 
+            mMenu : mMenuArr[i]
+            , sMenuArr : sMenuArr.filter(menu => menu.MCodeName == mMenuArr[i].MCodeName)
+        };
+        */
+        var curMmenu = { 
+            mMenu : mMenuArr[i]
+            , sMenuArr: sMenuArr.filter(function (menu) { return menu.MCodeName == mMenuArr[i].MCodeName })
+        };
+        resultMmenuArr.push(curMmenu);
+    }
+
+    return resultMmenuArr;
+}
+
+// 중메뉴 목록을 대응하는 대메뉴 목록에 추가
+navbarInitializer.appendMmenusToLmenus = function (mMenuArr, lMenuArr) {
+    var resultLmenuArr = [];
+
+    for(var i = 0; i<lMenuArr.length;i++)
+    {
+        var curLmenu = { 
+            lMenu : lMenuArr[i]
+            , mMenuArr: mMenuArr.filter(function (menu) { return menu.mMenu.LCodeName == lMenuArr[i].LCodeName })
+        };
+        resultLmenuArr.push(curLmenu);
+        /*
+        var curLmenu = { 
+            lMenu : lMenuArr[i]
+            , mMenuArr : mMenuArr.filter(menu => menu.mMenu.LCodeName == lMenuArr[i].LCodeName)
+        };
+        resultLmenuArr.push(curLmenu);
+        */
+    }
+
+    return resultLmenuArr;
+}
+
+// 주어진 menuArr 을 GNB 에 덧붙여서 화면에 표시
+navbarInitializer.appendLmenusToNavbar = function (menuArr) {
+    var menuHtml = '';
+    // 대메뉴 추가
+    for(var lIndex = 0; lIndex<menuArr.length;lIndex++){
+        menuHtml += '<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">'
+            + menuArr[lIndex].lMenu.MenuName + ' <b class="caret"></b></a>'
+            + '<ul class="dropdown-menu" role="menu">';
+
+        // 중메뉴 추가
+        var mMenuArr = menuArr[lIndex].mMenuArr;
+        for(var mIndex = 0; mIndex<mMenuArr.length;mIndex++){
+            var curMmenu = mMenuArr[mIndex].mMenu; // current middle menu
+
+            menuHtml += '<li class="dropdown-submenu"><a href="#">'
+                + curMmenu.MenuName + ' <i class="icon-arrow-right"></i></a>'
+                + '<ul class="dropdown-menu sub-menu">';
+
+            // 소메뉴 추가
+            var sMenuArr = mMenuArr[mIndex].sMenuArr;
+
+            if (curMmenu.LeafIs == 1) {
+                // 중메뉴가 leaf 인 경우, 중메뉴를 소메뉴에 추가
+                menuHtml += '<li><a class="gnb-menulink" href="'+curMmenu.MenuUrl+'" style="'+ curMmenu.MenuUrl +'">'
+                        + curMmenu.MenuName +  '</a></li>';
+            }
+            else if (sMenuArr.length == 1 && sMenuArr[0].MenuName == "옥션"){
+                // 소메뉴가 옥션 뿐인 경우, 소메뉴 URL & 중메뉴 이름으로 소메뉴 추가
+                menuHtml += '<li><a class="gnb-menulink" href="' + sMenuArr[0].MenuUrl + '" style="' + sMenuArr[0].MenuUrl + '">'
+                        + curMmenu.MenuName+'</a></li>';
+            }
+            else{
+                // 소메뉴가 여러 개인 경우, 소메뉴 URL & 소메뉴 이름으로 소메뉴 추가
+                for (var sIndex = 0; sIndex < sMenuArr.length; sIndex++) {
+                    menuHtml += '<li><a class="gnb-menulink" href="'+ sMenuArr[sIndex].MenuUrl+'" style="'+ sMenuArr[sIndex].MenuUrl +'">'
+                        + sMenuArr[sIndex].MenuName+ '</a></li>';
+                }
+            }
+
+            // 중메뉴 목록 닫음
+            menuHtml += '</ul></li>';
+        }
+
+        // 대메뉴 목록 닫음
+        menuHtml += '</ul></li>';
+    }
+ 
+    $('#globalNavigationBarMenus').append(menuHtml);
+}
+
+// GNB 에서 메뉴를 선택하여 해당 메뉴로 이동
+navbarInitializer.openMenu = function (event) {
+	var $anchor = $(event.target).eq(0);
+
+	// 휠을 누른 경우 새 탭에서 열도록 설정
+    //if(event.which == 2) $anchor.attr('target', '_blank');
+
+    //var param = "url=" + $anchor.attr('href');
+/*
+    $.ajax({
+        url: "/Menu/CheckMenuPermission?" + param,
+        type: "GET",
+        async: false,
+        contentType: "application/json; charset=utf-8",
+        success: function (result) {
+            if (result == 'false') {
+                alert('메뉴 권한이 없습니다. 메뉴 권한을 신청 해 주세요.');
+                event.preventDefault();
+            }
+            // else 수행 할 작업 없음
+        },
+        error: function (err) {
+            alert('메뉴 권한 조회에 실패 하였습니다.\n잠시 후 다시 시도 해 주세요.\n' + err.responseText);
+            event.preventDefault();
+        }
+    });*/
+
+	$(location).attr('pathname', $anchor.attr('style'));
+}
+
+$(document).ready(function () {
+    navbarInitializer.initNavbar();
+});
---
+++ template/MobileClose_files/comSetting.js
@@ -0,0 +1,330 @@
+﻿var carlendar_setting = {
+    animationType: 'fade',
+    allowKeyboardDelete: true,
+    clearString: 'Clean',
+    textAlign: 'center',
+    width: '100px',
+    height: 20,
+    dropDownHorizontalAlignment: 'right',
+    culture: 'ko-KR',
+    formatString: 'yyyy-MM-dd'
+};
+
+var pg_gubn_setting = {
+    source: [{ pg_code: '', pg_name: '전체' }, { pg_code: '1', pg_name: 'MOBILIANS' }, { pg_code: '2', pg_name: 'LGUPLUS' }],
+    displayMember: "pg_name",
+    valueMember: "pg_code",
+    selectedIndex: 0,
+    width: 100,
+    height: 20
+};
+
+var pg_gubn_with_daysum_setting = {
+    source: [{ pg_code: '', pg_name: '전체' }, { pg_code: '1', pg_name: 'MOBILIANS' }, { pg_code: '2', pg_name: 'LGUPLUS' }, { pg_code: '9998', pg_name: '일별소개' }],
+    displayMember: "pg_name",
+    valueMember: "pg_code",
+    selectedIndex: 0,
+    width: 100,
+    height: 20
+};
+
+
+var pay_type_setting = {
+    source: [{ pay_type: '1', pay_name: '승인일' }, { pay_type: '2', pay_name: '입금일' }],
+    displayMember: "pay_name",
+    valueMember: "pay_type",
+    selectedIndex: 0,
+    width: 100,
+    height: 20
+};
+
+
+var close_code_setting = {
+    source: [{ close_code: '', close_name: '승인일' }, { close_code: '11', close_name: '당일미확인' }, { close_code: '13', close_name: '당일상계' }, { close_code: '12', close_name: '이전미확인' }, { close_code: '14', close_name: '이전상계' }],
+    displayMember: "close_name",
+    valueMember: "close_code",
+    selectedIndex: 0,
+    width: 80,
+    height: 20
+};
+
+var same_code_setting = {
+    source: [{ close_code: '', close_name: '전체' }, { close_code: '1', close_name: '불일치' }, { close_code: '2', close_name: '처리완료' }],
+    displayMember: "close_name",
+    valueMember: "close_code",
+    selectedIndex: 0,
+    width: 80,
+    height: 20
+};
+
+
+var payment_line_setting = {
+    source: [{ line_code: '', line_name: '전체' }, { line_code: '1', line_name: '모바일' }, { line_code: '2', line_name: '신용카드' }, { line_code: '3', line_name: '계좌이체' }, { line_code: '2', line_name: '무통장입금' }],
+    displayMember: "line_name",
+    valueMember: "line_code",
+    selectedIndex: 0,
+    width: 100,
+    height: 20
+};
+
+var use_is_setting = {
+    source: [{ line_code: '', line_name: '전체' },{ line_code: 'Y', line_name: '사용' }, { line_code: 'N', line_name: '미사용' }],
+    displayMember: "line_name",
+    valueMember: "line_code",
+    selectedIndex: 0,
+    width: 100,
+    height: 20
+};
+
+var fee_type_setting = {
+    source: [{ line_code: '', line_name: '전체' }, { line_code: '101', line_name: '정액' }, { line_code: '102', line_name: '정률' }],
+    displayMember: "line_name",
+    valueMember: "line_code",
+    selectedIndex: 0,
+    width: 100,
+    height: 20
+};
+
+var payment_pay_method_setting_input = {
+    source: [{ line_code: '', line_name: '선택하세요.' }, { line_code: '1', line_name: '모바일' }, { line_code: '2', line_name: '신용카드' }, { line_code: '3', line_name: '계좌이체' }, { line_code: '4', line_name: '무통장입금' }],
+    displayMember: "line_name",
+    valueMember: "line_code",
+    selectedIndex: 0,
+    width: 100,
+    height: 20
+};
+
+var pay_type_setting_input = {
+    source: [{ pg_code: '', pg_name: '선택하세요' }, { pg_code: '1', pg_name: 'MOBILIANS' }, { pg_code: '2', pg_name: 'LGUPLUS' }],
+    displayMember: "pg_name",
+    valueMember: "pg_code",
+    selectedIndex: 0,
+    width: 100,
+    height: 20
+};
+
+var fee_type_setting_input = {
+    source: [{ line_code: '', line_name: '선택하세요' }, { line_code: '101', line_name: '정액' }, { line_code: '102', line_name: '정률' }],
+    displayMember: "line_name",
+    valueMember: "line_code",
+    selectedIndex: 0,
+    width: 100,
+    height: 20
+};
+
+var use_yn_setting_input = {
+    source: [ { line_code: 'Y', line_name: '사용' }, { line_code: 'N', line_name: '미사용' }],
+    displayMember: "line_name",
+    valueMember: "line_code",
+    selectedIndex: 0,
+    width: 100,
+    height: 20
+};
+
+var processCode =
+{
+    source: [{ close_code: '', close_name: '전체' }, { close_code: '3', close_name: '주문환불' }, { close_code: '0', close_name: '미처리' }, { close_code: '2', close_name: '전문취소' }],
+    displayMember: "close_name",
+    valueMember: "close_code",
+    selectedIndex: 0,
+    width: 80,
+    height: 20
+};
+
+
+var searchCode =
+{
+    source: [{ close_code: '0', close_name: '승인번호' }, { close_code: '1', close_name: '주문번호' }, { close_code: '2', close_name: '배송비결제번호' }, { close_code: '3', close_name: '휴대폰번호' }, { close_code: '4', close_name: '고객ID' }, { close_code: '5', close_name: '판매자ID' }],
+    displayMember: "close_name",
+    valueMember: "close_code",
+    selectedIndex: 0,
+    width: 100,
+    height: 20
+};
+
+
+var refund_close_setting = {
+    source: [{ close_code: '', close_name: '환불완료일' }, { close_code: '11', close_name: '당일미확인' },  { close_code: '12', close_name: '이전미확인' }],
+    displayMember: "close_name",
+    valueMember: "close_code",
+    selectedIndex: 0,
+    width: 80,
+    height: 20
+};
+
+
+var refund_close_setting2 = {
+    source: [{ close_code: '', close_name: '승인일(원승인일)' }, { close_code: '1', close_name: 'Smile Cash 적립일' }],
+    displayMember: "close_name",
+    valueMember: "close_code",
+    selectedIndex: 0,
+    width: 120,
+    height: 20
+};
+
+var refund_processCode =
+{
+    source: [{ close_code: '', close_name: '전체' }, { close_code: '0', close_name: '미처리' }, { close_code: '1', close_name: '처리확인' }],
+    displayMember: "close_name",
+    valueMember: "close_code",
+    selectedIndex: 0,
+    width: 80,
+    height: 20
+};
+
+var refund_processCode2 =
+{
+    source: [{ close_code: '', close_name: '전체' }, { close_code: '0', close_name: '입금환불' }, { close_code: '1', close_name: '주문환불' }],
+    displayMember: "close_name",
+    valueMember: "close_code",
+    selectedIndex: 0,
+    width: 80,
+    height: 20
+};
+
+var settingDayDate = function (num) {
+    var settingDate = new Date();
+    settingDate.setDate(settingDate.getDate() + num);
+    return settingDate;
+}
+
+var settingDayDate_string  = function (num) {
+    var settingDate = new Date();
+    settingDate.setDate(settingDate.getDate() + num);
+
+
+    return settingDate.getFullYear() + "/" + (settingDate.getMonth() + 1) + "/" + (settingDate.getDate()) + " " + settingDate.getHours() + ":" + settingDate.getMinutes() + ":" + settingDate.getSeconds();
+}
+
+
+
+var cellsrenderer = function (index, datafield, value, defaultHtml, column, rowdata) {
+    var element = $(defaultHtml);
+    if (rowdata['STATUS_CODE'] == '999') {
+        element.css({ 'background-color': '#e0e9f5', 'width': '100%', 'height': '100%', 'padding': '5px 2px 0 0', 'margin': '0' });
+        return element[0].outerHTML;
+    }
+
+    if (rowdata['PAY_TYPE'] == '999') {
+        element.css({ 'background-color': '#477396', 'width': '100%', 'height': '100%', 'padding': '5px 2px 0 0', 'margin': '0' });
+        return element[0].outerHTML;
+    }
+    return defaultHtml;
+}
+
+
+var aggregates =
+    [{
+        'sum': function (aggregatedValue, currentValue, column, record) {
+            if (column == "CLOSE_DATE") {
+                return "전체합계";
+            } else {
+                if (record['STATUS_CODE'] == '999') {
+                    aggregatedValue = aggregatedValue + currentValue;
+                }
+                return aggregatedValue;
+            }
+        }
+    }];
+var aggregatesrenderer = function (aggregates) {
+    var renderstring = "";
+
+    $.each(aggregates, function (key, value) {
+        renderstring += '<div style="position: relative; margin: 4px; overflow: hidden;">' + value + '</div>';
+    });
+    return renderstring;
+}
+
+var localizationobj = {
+    // separator of parts of a date (e.g. '/' in 11/05/1955)
+    '/': "/",
+    // separator of parts of a time (e.g. ':' in 05:44 PM)
+    ':': ":",
+    // the first day of the week (0 = Sunday, 1 = Monday, etc)
+    firstDay: 0,
+    days: {
+        // full day names
+        names: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+        // abbreviated day names
+        namesAbbr: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+        // shortest day names
+        namesShort: ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"]
+    },
+    months: {
+        // full month names (13 months for lunar calendards -- 13th month should be "" if not lunar)
+        names: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December", ""],
+        // abbreviated month names
+        namesAbbr: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec", ""]
+    },
+    // AM and PM designators in one of these forms:
+    // The usual view, and the upper and lower case versions
+    //      [standard,lowercase,uppercase]
+    // The culture does not use AM or PM (likely all standard date formats use 24 hour time)
+    //      null
+    AM: ["AM", "am", "AM"],
+    PM: ["PM", "pm", "PM"],
+    eras: [
+    // eras in reverse chronological order.
+    // name: the name of the era in this culture (e.g. A.D., C.E.)
+    // start: when the era starts in ticks (gregorian, gmt), null if it is the earliest supported era.
+    // offset: offset in years from gregorian calendar
+    { "name": "A.D.", "start": null, "offset": 0 }
+    ],
+    twoDigitYearMax: 2029,
+    patterns: {
+        // short date pattern
+        d: "M/d/yyyy",
+        // long date pattern
+        D: "dddd, MMMM dd, yyyy",
+        // short time pattern
+        t: "h:mm tt",
+        // long time pattern
+        T: "h:mm:ss tt",
+        // long date, short time pattern
+        f: "dddd, MMMM dd, yyyy h:mm tt",
+        // long date, long time pattern
+        F: "dddd, MMMM dd, yyyy h:mm:ss tt",
+        // month/day pattern
+        M: "MMMM dd",
+        // month/year pattern
+        Y: "yyyy MMMM",
+        // S is a sortable format that does not vary by culture
+        S: "yyyy\u0027-\u0027MM\u0027-\u0027dd\u0027T\u0027HH\u0027:\u0027mm\u0027:\u0027ss"
+    },
+    percentsymbol: "%",
+    currencysymbol: "$",
+    currencysymbolposition: "before",
+    decimalseparator: '.',
+    thousandsseparator: ',',
+    pagergotopagestring: "Go to page:",
+    pagershowrowsstring: "Show rows:",
+    pagerrangestring: " of ",
+    pagerpreviousbuttonstring: "previous",
+    pagernextbuttonstring: "next",
+    groupsheaderstring: "Drag a column and drop it here to group by that column",
+    sortascendingstring: "Sort Ascending",
+    sortdescendingstring: "Sort Descending",
+    sortremovestring: "Remove Sort",
+    groupbystring: "Group By this column",
+    groupremovestring: "Remove from groups",
+    filterclearstring: "Clear",
+    filterstring: "Filter",
+    filtershowrowstring: "Show rows where:",
+    filtershowrowdatestring: "Show rows where date:",
+    filterorconditionstring: "Or",
+    filterandconditionstring: "And",
+    filterselectallstring: "(Select All)",
+    filterchoosestring: "Please Choose:",
+    filterstringcomparisonoperators: ['empty', 'not empty', 'contains', 'contains(match case)',
+        'does not contain', 'does not contain(match case)', 'starts with', 'starts with(match case)',
+        'ends with', 'ends with(match case)', 'equal', 'equal(match case)', 'null', 'not null'],
+    filternumericcomparisonoperators: ['equal', 'not equal', 'less than', 'less than or equal', 'greater than', 'greater than or equal', 'null', 'not null'],
+    filterdatecomparisonoperators: ['equal', 'not equal', 'less than', 'less than or equal', 'greater than', 'greater than or equal', 'null', 'not null'],
+    filterbooleancomparisonoperators: ['equal', 'not equal'],
+    validationstring: "Entered value is not valid",
+    emptydatastring: "조회된 데이터가 없습니다.",
+    filterselectstring: "Select Filter",
+    loadtext: "조회중...",
+    clearstring: "Clear",
+    todaystring: "Today"
+};
